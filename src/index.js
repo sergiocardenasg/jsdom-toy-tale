4cce3502e1b3d1b09b67830b093b457628836c47
sergiocardenasg <sergiocardenas@utexas.edu>
2021-04-15 21:50:34 -0500

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,49 @@
 let addToy = false;
+const baseURL = "http://localhost:3000/toys";
 
 document.addEventListener("DOMContentLoaded", () => {
+  const toyCollection = document.querySelector("#toy-collection");
+  
+  fetch(baseURL)
+  .then(resp => resp.json())
+  .then(data => {
+    data.forEach(createNewToy)
+  })
+
+  const createNewToy = (toy) => {
+    const newDiv = document.createElement("div"), 
+    imgTag = document.createElement("img"),
+    hTag = document.createElement("h2"),
+    pTag = document.createElement("p"),
+    bttn = document.createElement("button")
+
+    newDiv.className = "card";
+    hTag.innerText = toy.name;
+    imgTag.src = `${toy.image}`;
+    imgTag.className = "toy-avatar";
+    pTag.innerText = toy.likes;
+    bttn.innerText = "Like"
+    bttn.className = "like-btn"
+
+    newDiv.append(hTag, imgTag, pTag, bttn);
+    toyCollection.append(newDiv)
+
+    bttn.addEventListener('click', function(){
+      let likes = Number(pTag.innerText) + 1
+      fetch(`${baseURL}/${toy.id}`, {
+        method: "PATCH",
+        headers: {
+          "Content-Type" : "application/json"
+        },
+        body:JSON.stringify({
+          likes
+        })
+      })
+      pTag.innerText = likes;
+    });
+  }
+
+
   const addBtn = document.querySelector("#new-toy-btn");
   const toyFormContainer = document.querySelector(".container");
   addBtn.addEventListener("click", () => {
